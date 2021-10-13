343f3481e454bd69338bf006e1aec02f511e91b2
smhrd <smhrd@DESKTOP-7O78N8D>
2021-10-13 14:24:45 +0900

    오류 수정 및 세션으로 로그인한 아이디를 가져오게 설정

--- LarvaPro/src/main/java/com/controller/MessageService.java
+++ LarvaPro/src/main/java/com/controller/MessageService.java
@@ -18,6 +18,8 @@ import com.model.memberVO;
 @WebServlet("/MessageService")
 public class MessageService extends HttpServlet {
 	
+	private static final long serialVersionUID = 1L;
+
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		//세션 요청
